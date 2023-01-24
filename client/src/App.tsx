7190970b2719817a433f5dcbcc297599b7cf02bd
Wilson Alfonso Gomez Santiago <wilsonscout20@gmail.com>
2023-01-23 21:34:18 -0400

    Fixing some styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -24,7 +24,7 @@ const App = () => {
         Create
       </Link>
     </header>
-    <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
+    <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)] h-[100%]'>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/create-post' element={<CreatePost />} />
