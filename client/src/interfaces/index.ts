b9db41b0355c629f068c69599f2c23a3203c5782
Wilson Alfonso Gomez Santiago <wilsonscout20@gmail.com>
2023-01-23 21:33:25 -0400

    Exporting and hard-coding the options into arrays divided by options category.

--- client/src/interfaces/index.ts
+++ client/src/interfaces/index.ts
@@ -1,9 +1,13 @@
 import Post from "./Post";
 import Form from "./Form";
 import IFormField from "./FormField";
+import IPromptOptionsSection from './IPromptOptionsSection';
+import IPromptOption from "./IPromptOption";
 
 export type {
     Post,
     Form,
-    IFormField
+    IFormField,
+    IPromptOptionsSection,
+    IPromptOption
 }
---
+++ client/src/utils/promptOptions.ts
@@ -0,0 +1,197 @@
+import { IPromptOption } from '../interfaces';
+
+const designOptions: IPromptOption[] = [
+    {
+        name:'standard',
+        value: 'standard style',
+        selected: false
+    },
+    {
+        name:'Japanese anime',
+        value: 'Japanese anime style',
+        selected: false
+    },
+    {
+        name:'Pixar movie',
+        value: 'Pixar movie style',
+        selected: false
+    },
+    {
+        name:'cyber punk',
+        value: 'cyber punk style',
+        selected: false
+    },
+    {
+        name:'steam punk',
+        value: 'steam punk style',
+        selected: false
+    },
+    {
+        name:'waterhouse',
+        value: 'waterhouse style',
+        selected: false
+    },
+    {
+        name:'bloodborne',
+        value: 'bloodborne style',
+        selected: false
+    },
+    {
+        name:'grunge',
+        value: 'grunge style',
+        selected: false
+    },
+]
+
+const artistOptions: IPromptOption[] = [
+    {
+        name:'Andy Warhol',
+        value: 'Andy Warhol style',
+        selected: false
+    },
+    {
+        name:'Da Vinci anime',
+        value: 'Da Vinci anime style',
+        selected: false
+    },
+    {
+        name:'Salvador Dali',
+        value: 'Salvador Dali style',
+        selected: false
+    },
+    {
+        name:'Picasso',
+        value: 'Picasso style',
+        selected: false
+    }
+]
+
+const lightingOptions: IPromptOption[] = [
+    {
+        name:'volumetric',
+        value: 'volumetric lightning style',
+        selected: false
+    },
+    {
+        name:'cinematic',
+        value: 'cinematic lightning style',
+        selected: false
+    },
+    {
+        name:'octane',
+        value: 'octane lightning style',
+        selected: false
+    },
+    {
+        name:'softbox',
+        value: 'softbox lightning style',
+        selected: false
+    },
+    {
+        name:'glowing lights',
+        value: 'glowing lights lightning style',
+        selected: false
+    },
+    {
+        name:'blue',
+        value: 'blue lightning style',
+        selected: false
+    },
+    {
+        name:'long exposure',
+        value: 'long exposure lightning style',
+        selected: false
+    },
+    {
+        name:'fairy',
+        value: 'fairy lightning style',
+        selected: false
+    },
+]
+
+const aspectRatioOptions: IPromptOption[] = [
+    //Standard Ratio
+    {
+        name:'1:1',
+        value: '--ar 1:1',
+        selected: true
+    },
+    //Portrait
+    {
+        name:'9:16',
+        value: '--ar 9:16',
+        selected: false
+    },
+    {
+        name:'4:5',
+        value: '--ar 4:5',
+        selected: false
+    },
+    {
+        name:'3:4',
+        value: '--ar 3:4',
+        selected: false
+    },
+    {
+        name:'2:3',
+        value: '--ar 2:3',
+        selected: false
+    },
+    {
+        name:'10:16',
+        value: '--ar 10:16',
+        selected: false
+    },
+    //Landscape
+    {
+        name:'16:9',
+        value: '--ar 16:9',
+        selected: false
+    },
+    {
+        name:'5:4',
+        value: '--ar 5:4',
+        selected: false
+    },
+    {
+        name:'4:3',
+        value: '--ar 4:3',
+        selected: false
+    },
+    {
+        name:'3:2',
+        value: '--ar 3:2',
+        selected: false
+    }
+]
+
+const chaosOption: IPromptOption = {
+    name: "chaos",
+    value:"--chaos ",
+    maxValue: 100,
+    selected: false
+}
+
+const stylizeOption: IPromptOption = {
+    name: "Stylize",
+    value:"--stylize ",
+    maxValue: 60000,
+    selected: false
+}
+
+const qualityOption: IPromptOption = {
+    name: "Quality",
+    value:"--quality ",
+    maxValue: 5,
+    selected: false
+}
+
+export {
+    designOptions,
+    artistOptions,
+    lightingOptions,
+    aspectRatioOptions,
+    chaosOption, 
+    stylizeOption,
+    qualityOption
+}
